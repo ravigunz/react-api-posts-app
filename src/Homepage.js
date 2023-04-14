3c1dac64a12689e2ca2fa608f43fd7b1f3a3ba1c
Ravi Gunasena <ravigunz@gmail.com>
2023-04-14 12:08:39 +0530

    add project

---
+++ src/Homepage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Posts from './layouts/Posts'
+
+export default function Homepage() {
+  return (
+    <div>
+        <Posts />
+    </div>
+  )
+}
---
+++ src/layouts/Posts.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Posts() {
+
+    const [posts, setPosts] = useState([]);
+
+  return (
+    <div>
+        
+    </div>
+  )
+}
